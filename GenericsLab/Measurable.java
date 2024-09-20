b867e818f4841f1e93c068c6f6d6636a827244b5
danielgetahun100 <Dgetahun60@gmail.com>
2024-09-20 15:41:10 -0400

    Add files via upload

---
+++ GenericsLab/Measurable.java
@@ -0,0 +1,16 @@
+package generics_lab;
+
+/**
+Describes any class whose objects can be measured.
+*/
+public interface Measurable
+{
+/**
+   Computes the measure of the object.
+   @return the measure
+*/
+	double getMeasure() {
+	// TODO Auto-generated method stub
+	return 0;
+}
+}
